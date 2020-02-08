f9bd0eac266cfb367579620ffbf5fe7b91ef3cb9
AndersonAlmeida93 <andersonolld@gmail.com>
2020-02-08 20:22:12 -0300

    Projeto Calculadora Angular.

---
+++ e2e/src/app.e2e-spec.ts
@@ -0,0 +1,10 @@
+import { AppPage } from './app.po';
+import { browser, logging } from 'protractor';
+
+describe('calculadora App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+});
---
+++ src/app/calculadora/services/calculadora.service.ts
@@ -0,0 +1,57 @@
+/**
+ * Serviço responsável por executar as operações da calculadora.
+ * 
+ * @author Anderson Almeida <andersonolld@gmail.com>
+ * @since 1.0.0
+ */
+
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CalculadoraService {
+
+  /*
+  Define as constates utilizadas
+  para identificar as operações de cálculo
+  */
+static readonly SOMA: string = '+';
+static readonly SUBTRACAO: string = '-';
+static readonly DIVISAO: string = '/';
+static readonly MULTIPLICACAO: string = '*';
+
+  constructor() { }
+
+  /**
+   * Método que calcula uma operação matemática dado
+   * dois números.
+   * Suporte as operações soma,subtração, divisão e multiplicação.
+   * 
+   * @param num1 number
+   * @param num2 number
+   * @param operacao sting operação a ser executada
+   * @return number resultado da operação
+   */
+
+   calcular(num1: number , num2: number, operacao: string): number{
+
+    let resultado: number ;
+
+    switch(operacao){
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2;
+      break;
+      case CalculadoraService.SUBTRACAO:
+        resultado = num1 - num2;
+      break;
+      case CalculadoraService.DIVISAO:
+        resultado = num1 / num2;
+      break;
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = num1 * num2;
+      break;
+      default:
+        resultado = 0;
+    }
+    return resultado;
+   }
+}
